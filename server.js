ef5d63d4d2398843f36774cb41d8a9939337c49b
realitylottery <56432596+realitylottery@users.noreply.github.com>
2025-08-08 15:54:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ mongoose.connect("mongodb+srv://realitylottery:Moataz1234@realitylottery.fzcf67p
 
 // Health check or default route
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'workspace', 'index.html'));
+  res.sendFile(path.join(__dirname, 'main', 'index.html'));
 });
 
 // إرسال الدفع
@@ -152,5 +152,6 @@ app.post("/api/approve-payment", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
 });
+
 
 
