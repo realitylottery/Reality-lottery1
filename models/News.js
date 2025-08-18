e8180bfee9618c3c3abf3d8e67e56caadd2508ce
realitylottery <56432596+realitylottery@users.noreply.github.com>
2025-08-18 19:46:04 +0300

    Update News.js

--- models/News.js
+++ models/News.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
 
+const NewsSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  text: { type: String },
+  image: { type: String },
+  buttonLabel: { type: String },
+  buttonUrl: { type: String },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('News', NewsSchema);
