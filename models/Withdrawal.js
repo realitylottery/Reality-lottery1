e146f8f71f5cfa8de33dbfb618603ae0d5b18c43
realitylottery <56432596+realitylottery@users.noreply.github.com>
2025-08-20 07:17:55 +0300

    Update Withdrawal.js

--- models/Withdrawal.js
+++ models/Withdrawal.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const WithdrawalSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'UserId', required: true },
   amount: { type: Number, required: true },
   walletAddress: { type: String, required: true },
   status: { type: String, enum: ['pending', 'approved', 'rejected'], default: 'pending' },
