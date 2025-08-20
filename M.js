9501446d1734bbb90e25829ebad4907fb69757bb
realitylottery <56432596+realitylottery@users.noreply.github.com>
2025-08-20 16:28:59 +0300

    Update M.js

--- M.js
+++ M.js
@@ -1,7 +1,7 @@
 // createAdmin.js
 require("dotenv").config();
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const User = require("./models/User");
 
 (async () => {
