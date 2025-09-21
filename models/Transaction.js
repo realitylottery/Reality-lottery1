83349f2c06e13c487338cca95a07d33290de44ac
realitylottery <56432596+realitylottery@users.noreply.github.com>
2025-09-21 05:17:20 +0300

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const transactionSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   amount: { type: Number, required: true },
   type: { type: String, required: true }, // TASK_REWARD_CLAIM, TASK_REWARD, etc.
   description: { type: String },
